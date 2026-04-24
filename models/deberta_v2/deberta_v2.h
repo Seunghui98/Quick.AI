0c6853f52778ec0cc8de00176c1ac9880577ab81
Ellen <54658745+Seunghui98@users.noreply.github.com>
2026-04-24 13:45:21 +0900

    [deberta_v2] Update root meson and SentenceTransformer/DebertaV2 headers

---
+++ models/deberta_v2/deberta_v2.h
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2026 Seunghui Lee <shsh1004.lee@samsung.com>
+ *
+ * @file   debertav2.h
+ * @date   14 January 2026
+ * @see    https://github.com/nntrainer/nntrainer
+ * @author Seunghui Lee <shsh1004.lee@samsung.com>
+ * @bug    No known bugs except for NYI items
+ * @note  Please refer to the following code :
+ * https://github.com/huggingface/transformers/blob/5c1c72b/src/transformers/models/deberta/modeling_deberta.py
+ */
+
+#ifndef __DEBERTA_V2_H__
+#define __DEBERTA_V2_H__
+
+#include <sentence_transformer.h>
+#include <transformer.h>
+
+namespace quick_dot_ai {
+
+/**
+ * @brief DebertaV2 class
+ */
+class DebertaV2 : public SentenceTransformer {
+
+public:
+  static constexpr const char *architectures = "DebertaV2";
+
+  DebertaV2(json &cfg, json &generation_cfg, json &nntr_cfg) :
+    Transformer(cfg, generation_cfg, nntr_cfg, ModelType::EMBEDDING),
+    SentenceTransformer(cfg, generation_cfg, nntr_cfg) {
+    setupParameters(cfg, generation_cfg, nntr_cfg);
+  }
+
+  virtual ~DebertaV2() = default;
+
+protected:
+  /**
+   * @brief Construct Model
+   */
+  void constructModel() override;
+
+  std::vector<LayerHandle> createDebertaLayer(const int layer_id,
+                                              std::string input_name,
+                                              std::string rel_embeddings_name);
+
+  std::vector<LayerHandle>
+  createDebertaV2Attention(const int layer_id, std::string input_name,
+                           std::string rel_embeddings_name);
+
+  /**
+   * @brief Setup the parameters for the Deberta V2 model
+   */
+  void setupParameters(json &cfg, json &generation_cfg,
+                       json &nntr_cfg) override;
+
+  /**
+   * @brief register CustomLayers
+   */
+  void registerCustomLayers() override;
+
+  int MAX_RELATIVE_POSITIONS;
+  bool C2P;
+  bool P2C;
+  bool SHARE_ATT_KEY;
+  bool RELATIVE_ATTENTION;
+  int POSITION_BUCKETS;
+};
+
+} // namespace quick_dot_ai
+
+#endif /* __DEBERTA_V2_H__ */
